3a9a8b717440f2ade11b71c231763dc3159f2c40
kinzart <kinzartmusica@gmail.com>
2023-09-19 19:43:03 -0300

    first commit

---
+++ listar_dados.php
@@ -0,0 +1,49 @@
+<?php
+$servername = "localhost";
+$username = "u906429557_kinzart";
+$password = "0Mp#UZ>7>!ebc2t16780o1";
+$dbname = "u906429557_formspot";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Conexão falhou: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM apresentacoes";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    echo "<table>";
+    echo "<tr><th>Imagem</th><th>Nome do Artista</th><th>Título da Apresentação</th><th>Descrição</th><th>Gêneros</th><th>Integrantes no IG</th><th>Orçamento Final</th></tr>";
+    while ($row = $result->fetch_assoc()) {
+        echo "<tr>";
+        // Decodifique o JSON para obter os caminhos das imagens
+        $caminhos_imagens = json_decode($row["fotos_divulgacao"]);
+
+        if (!empty($caminhos_imagens)) {
+            // Suponha que você deseje exibir a primeira imagem da lista
+            for ($i = 0; $i < min(3, count($caminhos_imagens)); $i++) {
+                echo "<td><a href='" . $caminhos_imagens[$i] . "' download><img src='" . $caminhos_imagens[$i] . "' width='100' height='100'</a> </td>";
+            }
+        } else {
+            echo "<td>Nenhuma imagem disponível</td>";
+        }
+        
+
+
+
+        echo "<td><strong>" . $row["nome_artista"] . "</strong></td>";
+        echo "<td>" . $row["titulo_apresentacao"] . "</td>";
+        echo "<td>" . $row["descricao_apresentacao"] . "</td>";
+        echo "<td>" . $row["generos"] . "</td>";
+        echo "<td>" . $row["integrantes_ig"] . "</td>";
+        echo "<td> R$ " . $row["orcamento_final"] . "</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+} else {
+    echo "Nenhum resultado encontrado.";
+}
+$conn->close();
+?>
---
+++ processar_formulario.php
@@ -0,0 +1,50 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "nova_senha";
+$dbname = "artist_form";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Conexão falhou: " . $conn->connect_error);
+}
+
+$nome_artista = $_POST['nome_artista'];
+$titulo_apresentacao = $_POST['titulo_apresentacao'];
+$descricao_apresentacao = $_POST['descricao_apresentacao'];
+$generos = $_POST['generos'];
+$integrantes_ig = $_POST['integrantes_ig'];
+$orcamento_final = $_POST['orcamento_final'];
+
+// Processar o upload de imagens
+$diretorio_upload = "imagens/";
+$caminhos_imagens = [];
+
+foreach ($_FILES['fotos_divulgacao']['tmp_name'] as $key => $tmp_name) {
+    $nome_arquivo = $_FILES['fotos_divulgacao']['name'][$key];
+    $caminho_destino = $diretorio_upload . $nome_arquivo;
+
+    if (move_uploaded_file($tmp_name, $caminho_destino)) {
+        $caminhos_imagens[] = $caminho_destino;
+    } else {
+        echo "Erro ao fazer upload da imagem $nome_arquivo.";
+    }
+}
+
+// Agora, $caminhos_imagens contém os caminhos das imagens carregadas
+
+// Inserir esses caminhos no banco de dados
+$caminhos_imagens_json = json_encode($caminhos_imagens);
+
+$sql = "INSERT INTO apresentacoes (nome_artista, titulo_apresentacao, descricao_apresentacao, generos, integrantes_ig, fotos_divulgacao, orcamento_final)
+        VALUES ('$nome_artista', '$titulo_apresentacao', '$descricao_apresentacao', '$generos', '$integrantes_ig', '$caminhos_imagens_json', '$orcamento_final')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Dados inseridos com sucesso!";
+} else {
+    echo "Erro ao inserir dados: " . $conn->error;
+}
+
+$conn->close();
+?>
