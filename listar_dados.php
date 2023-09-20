6ebb68abab31fcb9bec6585dfd3d4c4fb4c8a35f
kinzart <kinzartmusica@gmail.com>
2023-09-19 21:24:12 -0300

    11

--- listar_dados.php
+++ listar_dados.php
@@ -15,7 +15,7 @@ $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     echo "<table>";
-    echo "<tr><th>Imagem</th></tr>";
+    echo "<tr><th>Imagem</th><th>Nome do Artista</th><th>Título da Apresentação</th><th>Descrição</th><th>Gêneros</th><th>Integrantes no IG</th><th>Orçamento Final</th></tr>";
     while ($row = $result->fetch_assoc()) {
         echo "<tr>";
         // Decodifique o JSON para obter os caminhos das imagens
@@ -25,12 +25,11 @@ if ($result->num_rows > 0) {
             // Suponha que você deseje exibir a primeira imagem da lista
             for ($i = 0; $i < min(3, count($caminhos_imagens)); $i++) {
                 echo "<td><a href='" . $caminhos_imagens[$i] . "' download><img src='" . $caminhos_imagens[$i] . "' width='100' height='100'</a> </td>";
-          
             }
         } else {
             echo "<td>Nenhuma imagem disponível</td>";
         }
-       echo"<tr><th>Nome do Artista</th><th>Título da Apresentação</th><th>Descrição</th><th>Gêneros</th><th>Integrantes no IG</th><th>Orçamento Final</th></tr>""
+        
 
 
 
