76e9e1e36b772b7bd2abbef3063b1e77fa2ecdc0
kinzart <kinzartmusica@gmail.com>
2023-09-21 09:30:52 -0300

    testando imagens

--- processar_formulario.php
+++ processar_formulario.php
@@ -9,6 +9,21 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Conexão falhou: " . $conn->connect_error);
 }
+// Verifique se o array $_FILES contém dados
+if (!empty($_FILES['fotos_divulgacao']['name'])) {
+    // Loop pelos arquivos enviados
+    for ($i = 0; $i < count($_FILES['fotos_divulgacao']['name']); $i++) {
+        // Exiba informações sobre o arquivo
+        echo "Nome do arquivo: " . $_FILES['fotos_divulgacao']['name'][$i] . "<br>";
+        echo "Tipo do arquivo: " . $_FILES['fotos_divulgacao']['type'][$i] . "<br>";
+        echo "Tamanho do arquivo: " . $_FILES['fotos_divulgacao']['size'][$i] . " bytes<br>";
+        echo "Erro de envio: " . $_FILES['fotos_divulgacao']['error'][$i] . "<br>";
+    }
+} else {
+    echo "Nenhum arquivo foi enviado.";
+}
+
+// ...
 
 $nome_artista = $_POST['nome_artista'];
 $titulo_apresentacao = $_POST['titulo_apresentacao'];
@@ -18,7 +33,7 @@ $integrantes_ig = $_POST['integrantes_ig'];
 $orcamento_final = $_POST['orcamento_final'];
 
 // Processar o upload de imagens
-$diretorio_upload = "./imagens/";
+$diretorio_upload = "imagens/";
 $caminhos_imagens = [];
 
 foreach ($_FILES['fotos_divulgacao']['tmp_name'] as $key => $tmp_name) {
@@ -28,7 +43,7 @@ foreach ($_FILES['fotos_divulgacao']['tmp_name'] as $key => $tmp_name) {
     if (move_uploaded_file($tmp_name, $caminho_destino)) {
         $caminhos_imagens[] = $caminho_destino;
     } else {
-        echo "Erro ao fazer upload da imagem $nome_arquivo.";
+        echo "$_FILES['fotos_divulgacao']['error'][$key]";
     }
 }
 
